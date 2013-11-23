6424fedd7cd0455288c8f9d6efa142c2398766fb
Carlo Sciolla <skuro@skuro.tk>
2013-11-23 10:10:01 +0100

    Added missing file

---
+++ src/seeduml/plant_uml.clj
@@ -0,0 +1,23 @@
+(ns seeduml.plant-uml
+  (:require [seeduml.store :as store]))
+
+(def *pumls* (store/get-category "Plantuml"))
+
+(defn- get-puml-node [pad]
+  (store/one-from-category *plantumls* "pad" pad))
+
+(defn get-puml
+  "Retrieves a PlantUML source file from the store"
+  [pad]
+  (-> pad
+      get-puml-node
+      :data
+      :source))
+
+(defn store-puml
+  "Stores a new version of the plant uml source, or creates a new one"
+  [pad puml]
+  (if-let [stored-puml (get-puml-node pad)]
+    (store/update stored-puml {:source puml})
+    (store/create-in-category *pumls* {:source puml
+                                       :pad    pad} "is_a")))
