8c0fd4469790433c88c34ef72ad5a360c54121a4
Carlo Sciolla <skuro@skuro.tk>
2014-02-17 15:40:39 +0100

    Added the capability to override the config

--- src/seeduml/config.clj
+++ src/seeduml/config.clj
@@ -1,7 +1,14 @@
 (ns seeduml.config
   (:require [environ.core :as env]))
 
+(def overrides (atom {}))
+
+(defn setenv
+  "Overrides the configuration option at key with the provided value"
+  [key value]
+  (swap! overrides assoc key value))
+
 (defn getenv
   "Returns the value of the system property found at the given key"
   [key]
-  (env/env key))
+  ((merge @overrides env/env) key))
--- test/seeduml/test_config.clj
+++ test/seeduml/test_config.clj
@@ -8,3 +8,11 @@
         _     (System/setProperty value value) ; sets the sysprop
         _     (use '[environ.core] :reload)]   ; reloads the env
     (is (= value (config/getenv key)))))
+
+(deftest setenv-test
+  (let [test-key :__test_config]
+    (is (nil? (config/getenv test-key))
+        "The test key is already found in the config, cannot test")
+    (config/setenv test-key 42)
+    (is (= 42 (config/getenv test-key))
+        "The test key was not found in the config after setenv")))
