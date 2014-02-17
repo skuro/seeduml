f73b50f60ebb8e132a70255b30972f750472787e
Carlo Sciolla <skuro@skuro.tk>
2014-02-17 15:43:13 +0100

    Minor refactoring

--- test/seeduml/test_config.clj
+++ test/seeduml/test_config.clj
@@ -7,12 +7,16 @@
         key   (keyword value)
         _     (System/setProperty value value) ; sets the sysprop
         _     (use '[environ.core] :reload)]   ; reloads the env
-    (is (= value (config/getenv key)))))
+    (is (= value (config/getenv key))
+        "Cannot find a system property in the config")))
 
 (deftest setenv-test
   (let [test-key :__test_config]
+
+    (config/setenv test-key nil) ; clear the value, useful for continuous testing
     (is (nil? (config/getenv test-key))
         "The test key is already found in the config, cannot test")
+
     (config/setenv test-key 42)
     (is (= 42 (config/getenv test-key))
         "The test key was not found in the config after setenv")))
