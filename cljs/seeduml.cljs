21039ff303fe4e5f44bfaa9475228a094999cbda
Carlo Sciolla <skuro@skuro.tk>
2014-03-22 12:24:48 +0100

    No browser repl when live

--- cljs/seeduml.cljs
+++ cljs/seeduml.cljs
@@ -2,7 +2,7 @@
   (:require [clojure.browser.repl :as repl]
             [seeduml.editor :as editor]))
 
-(repl/connect "http://localhost:9000/repl")
+#_(repl/connect "http://localhost:9000/repl")
 
 (let [textArea (. js/document (getElementById "plantuml"))]
   (editor/init textArea))
